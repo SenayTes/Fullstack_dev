4a395f913d5e8ebe1098bb9a7376317a985d076e
SenayTes <eggm636@gmail.com>
2024-02-04 14:01:53 +0100

    Fixed register page

--- ProcastNoMore/ProcastNoMore/settings.py
+++ ProcastNoMore/ProcastNoMore/settings.py
@@ -117,8 +117,8 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / '"todo/static"']
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'todo/static/']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
